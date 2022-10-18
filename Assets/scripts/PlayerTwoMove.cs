62407b3bb624bf1cc9704ec64ecc401a6b3c1677
up2119604@myport.ac.uk <>
2022-10-18 19:13:51 +0100

    players

--- Assets/scripts/PlayerTwoMove.cs
+++ Assets/scripts/PlayerTwoMove.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerTwoMove : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float jumpForce = 10;
+    public GameObject p2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            rb.velocity = Vector2.up * jumpForce;
+            
+        }
     }
 }
