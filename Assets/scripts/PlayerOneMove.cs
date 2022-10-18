ad5ef7d2cabf8380551df01502a377d887eb4af5
up2119604@myport.ac.uk <>
2022-10-18 20:26:11 +0100

    changing the colour

--- Assets/scripts/PlayerOneMove.cs
+++ Assets/scripts/PlayerOneMove.cs
@@ -6,14 +6,16 @@ public class PlayerOneMove : MonoBehaviour
 {
     public Rigidbody2D rb1;
     public float jumpForceNormal = 10;
-    public float jumpForceBuff = 6;
+    public float jumpForceBuff = 10;
     public GameObject p1;
     public bool buffOnOrOff1 = false;
+    SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
         rb1 = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,15 @@ public class PlayerOneMove : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            buffOnOrOff1 = true;
+            if (buffOnOrOff1)
+            {
+                turningNormal1();
+            }
+            else
+            {
+                turningBuff1();
+            }
+            
         }
 
             //code for jumping, if the player is buff then the jump force is less
@@ -38,5 +48,20 @@ public class PlayerOneMove : MonoBehaviour
             
 
         }
+    }
+    void turningBuff1()
+    {
+        //all code for the squence of turning buff
+        buffOnOrOff1 = true;
+        rb1.gravityScale = 1.0f;
+        sprite.color = new Color(1, 0, 0, 1);
+
+    }
+    void turningNormal1()
+    {
+        //all code for the sequence of turing normal
+        buffOnOrOff1 = false;
+        rb1.gravityScale = 0.6f;
+        sprite.color = new Color(1, 1, 1, 1);
     }
 }
