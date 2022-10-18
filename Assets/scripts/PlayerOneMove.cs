1b6b8294cd7d1a743fd66bc1ec04431cb599650b
up2119604@myport.ac.uk <>
2022-10-18 20:51:29 +0100

    player 1 now has buff mode

--- Assets/scripts/PlayerOneMove.cs
+++ Assets/scripts/PlayerOneMove.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerOneMove : MonoBehaviour
 {
+    [SerializeField] private LayerMask platformslayerMask;
+
     public Rigidbody2D rb1;
-    public float jumpForceNormal = 10;
-    public float jumpForceBuff = 10;
     public GameObject p1;
+    public float jumpForceNormal = 10;
+    public float jumpForceBuff = 12;
     public bool buffOnOrOff1 = false;
     SpriteRenderer sprite;
 
@@ -23,37 +25,25 @@ public class PlayerOneMove : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (buffOnOrOff1)
-            {
-                turningNormal1();
-            }
-            else
-            {
-                turningBuff1();
-            }
-            
+            //if buffOnOrOff1 is true (meaning the player is buff)
+            //then player will change back to normal, else player turns buff
+            if (buffOnOrOff1) { turningNormal1();}
+            else { turningBuff1(); }
         }
 
-            //code for jumping, if the player is buff then the jump force is less
-            if (Input.GetKeyDown(KeyCode.W))
+        //code for jumping, if the player is buff then the jump force is less
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            if (buffOnOrOff1 == false)
-            {
-                rb1.velocity = Vector2.up * jumpForceNormal;
-            }
-            else
-            {
-                rb1.velocity = Vector2.up * jumpForceBuff;
-            }
-            
-
+            // if the player is not buff then the jump force will be normal
+            if (buffOnOrOff1 == false) { rb1.velocity = Vector2.up * jumpForceNormal; }
+            else { rb1.velocity = Vector2.up * jumpForceBuff; }
         }
     }
     void turningBuff1()
     {
         //all code for the squence of turning buff
         buffOnOrOff1 = true;
-        rb1.gravityScale = 1.0f;
+        rb1.gravityScale = 3.0f;
         sprite.color = new Color(1, 0, 0, 1);
 
     }
@@ -61,7 +51,7 @@ public class PlayerOneMove : MonoBehaviour
     {
         //all code for the sequence of turing normal
         buffOnOrOff1 = false;
-        rb1.gravityScale = 0.6f;
+        rb1.gravityScale = 1.0f;
         sprite.color = new Color(1, 1, 1, 1);
     }
 }
