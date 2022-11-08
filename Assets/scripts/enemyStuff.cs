cf80695849333bb22a808d76024fd44e4abb7045
up2119604@myport.ac.uk <>
2022-11-08 23:21:11 +0000

    bug fix

--- Assets/scripts/enemyStuff.cs
+++ Assets/scripts/enemyStuff.cs
@@ -6,10 +6,19 @@ public class enemyStuff : MonoBehaviour
 {
     public int maxHeath = 50;
     int currentHeath;
+    private bool facingRight = false;
+    public float movespeed = 5;
+    public GameObject player1;
+    public GameObject player2;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         currentHeath = maxHeath;
+        target = player1.transform;
     }
     public void takenDamage(int damage)
     {
@@ -34,10 +43,19 @@ public class enemyStuff : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("hit");
+    }
+    void Flip()
+    {
+        //code for fliping the sprite when moving left or right
+        facingRight = !facingRight;
+        transform.rotation = Quaternion.AngleAxis(180 - transform.eulerAngles.y, Vector3.up);
     }
 }
