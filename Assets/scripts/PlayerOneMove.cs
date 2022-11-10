15e7739f481bb779657ad7a7392a718f45b74f5b
debugproto@catpowered.net <quagsirus@catpowered.net>
2022-11-10 14:27:23 +0000

    rollback

--- Assets/scripts/PlayerOneMove.cs
+++ Assets/scripts/PlayerOneMove.cs
@@ -6,36 +6,7 @@ public class PlayerOneMove : MonoBehaviour
 {
     [SerializeField] Squirrel squirrel;
 
-    
-    public Rigidbody2D rb1;
-    public GameObject p1;
-    public GameObject acorn;
-    public float jumpForceNormal = 10;
-    public float jumpForceBuff1 = 12;
-    private float speed1 = 6;
-    public bool buffOnOrOff1 = false;
-    private float moveInput1;
-    private bool facingRight1 = true;
-    private bool isGrounded1;
-    public Transform groundCheck1;
-    public float checkRadius1;
-    public float attackRate = 2f;
-    float nextAttackTime = 0;
-
-    public LayerMask whatIsGround1;
-    SpriteRenderer sprite1;
-
-    public Animator animator;
-
-    // Start is called before the first frame update
-    
-    
-    public int currentHeath = 3;
-
-    
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         squirrel = gameObject.GetComponent<Squirrel>();
     }
@@ -61,54 +32,5 @@ public class PlayerOneMove : MonoBehaviour
         {
             squirrel.CancelBlock();
         }
-    }
-    void turningBuff1()
-    {
-        //all code for the squence of turning buff
-        buffOnOrOff1 = true;
-        rb1.gravityScale = 3.0f;
-        sprite1.color = new Color(1, 0, 0, 1);
-
-    }
-    void turningNormal1()
-    {
-        //all code for the sequence of turing normal
-        buffOnOrOff1 = false;
-        rb1.gravityScale = 2f;
-        sprite1.color = new Color(1, 1, 1, 1);
-    }
-    void Flip()
-    {
-        //code for fliping the sprite when moving left or right
-        facingRight1 =! facingRight1;
-        transform.rotation = Quaternion.AngleAxis(180 - transform.eulerAngles.y, Vector3.up);
-    }
-    void hitting()
-    {
-        // this has been moved to p1Punch script
-    }
-    //spawns the acorns which have there own logic
-    void throwing()
-    {
-        GameObject newAcorn = Instantiate(acorn, transform.position, Quaternion.identity);
-        if (facingRight1)
-        {
-            newAcorn.GetComponent<acornMov>().speed = 20;
-        }
-    }
-    public void takenDamage(int damage)
-    {
-        currentHeath -= damage;
-        Debug.Log("BANAANANA");
-        // add hurt animation here -------------
-        if (currentHeath <= 0)
-        {
-            death();
-        }
-    }
-    
-    void death()
-    {
-        //player is now dead
     }
 }
--- Assets/scripts/PlayerTwoMove.cs
+++ Assets/scripts/PlayerTwoMove.cs
@@ -10,39 +10,7 @@ public class PlayerTwoMove : MonoBehaviour
     bool isDespawned = true;
     float lastInteractionTime;
 
-    
-    public Rigidbody2D rb2;
-    public GameObject p2;
-    public GameObject acorn;
-    
-    public float jumpForceNormal2 = 10;
-    public float jumpForceBuff2 = 12;
-    private float speed2 = 6;
-    public bool buffOnOrOff2 = false;
-    private float moveInput2;
-    private bool facingRight2 = true;
-    private bool isGrounded2;
-    public Transform groundCheck2;
-    public float checkRadius2;
-    public float attackRate = 2f;
-    float nextAttackTime = 0;
-
-    public LayerMask whatIsGround2;
-    SpriteRenderer sprite2;
-
-    //public Animator animator;
-
-    // Start is called before the first frame update
-    
-    
-    
-    
-    int currentHeath = 3;
-    
-    //public Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         squirrel = gameObject.GetComponent<Squirrel>();
         animator = gameObject.GetComponent<Animator>();
@@ -95,56 +63,5 @@ public class PlayerTwoMove : MonoBehaviour
             animator.SetBool("isDisconnected", true);
             isDespawned = true;
         }
-    }
-    void turningBuff2()
-    {
-        //all code for the squence of turning buff
-        buffOnOrOff2 = true;
-        rb2.gravityScale = 3.0f;
-        sprite2.color = new Color(1, 0, 0, 1);
-
-    }
-    void turningNormal2()
-    {
-        //all code for the sequence of turing normal
-        buffOnOrOff2 = false;
-        rb2.gravityScale = 2.0f;
-        sprite2.color = new Color(1, 1, 1, 1);
-    }
-    void Flip()
-    {
-        //code for fliping the sprite when moving left or right
-        facingRight2 = !facingRight2;
-        Vector3 Scaler = transform.localScale;
-        Scaler.x *= -1;
-        transform.localScale = Scaler;
-    }
-    void hitting()
-    {
-        // this has been moved to p1Punch script
-    }
-    //spawns the acorns which have there own logic
-    void throwing()
-    {
-        GameObject newAcorn = Instantiate(acorn, transform.position, Quaternion.identity);
-        if (facingRight2)
-        {
-            newAcorn.GetComponent<acornMov>().speed = 20;
-        }
-    }
-    public void takenDamage(int damage)
-    {
-        currentHeath -= damage;
-        Debug.Log("BANAANANA");
-        // add hurt animation here -------------
-        if (currentHeath <= 0)
-        {
-            death();
-        }
-    }
-    
-    void death()
-    {
-        //death woop
     }
 }
