4cd4cc7c707d617a02cda64e943bb020120777e0
emmie.lily789@gmail.com <emmie.lily789@gmail.com>
2022-11-11 18:36:23 +0000

    game over pop up! :)

---
+++ Assets/scripts/gameOver.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour
+{
+    [SerializeField] [HideInInspector] Squirrel squirrel1;
+    [SerializeField] [HideInInspector] Squirrel squirrel2;
+
+    public GameObject gameoverUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squirrel1 = GameObject.Find("playerOne").GetComponent<Squirrel>();
+        squirrel2 = GameObject.Find("playerTwo").GetComponent<Squirrel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (squirrel1.isDead && squirrel2.isDead)
+        {
+            GameOver();
+        }
+        else if (squirrel1.isDead && squirrel2.isDespawned)
+        {
+            GameOver();
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        gameoverUI.SetActive(true);
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main");
+    }
+}
