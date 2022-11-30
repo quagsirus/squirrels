4f9e6eabc684322c6006ff599dbb5a7fe71ac522
emmie.lily789@gmail.com <emmie.lily789@gmail.com>
2022-11-30 23:13:24 +0000

    :)

--- Assets/scripts/UiStuff.cs
+++ Assets/scripts/UiStuff.cs
@@ -6,18 +6,46 @@ using UnityEngine.UI;
 
 public class UiStuff : MonoBehaviour
 {
+    public Image spriteRenderer;
+    public Sprite deadHeart;
+    public Sprite aliveHeart;
+
+    GameObject p1Lives, p2Lives, section;
+
     // Start is called before the first frame update
     void Start()
     {
+        p1Lives = GameObject.Find("p1 lives");
     }
     public void SetStat(string statistic, int amount)
     {
         TextMeshProUGUI statText = transform.Find(statistic).GetComponent<TextMeshProUGUI>();
         statText.text = char.ToUpper(statistic[0]) + statistic.Substring(1) + ": " + amount;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        if (statistic == "p1 Lives")
+        {
+            section = p1Lives;
+        }
+        else if (statistic == "p2 Lives")
+        {
+            section = p2Lives;
+        }
+        switch(amount)
+        {
+            case 3:
+                break;
+            case 2:
+                section.transform.Find("heart3").GetComponent<Image>().sprite = deadHeart;
+                break;
+            case 1:
+                section.transform.Find("heart2").GetComponent<Image>().sprite = deadHeart;
+                section.transform.Find("heart3").GetComponent<Image>().sprite = deadHeart;
+                break;
+            case 0:
+                section.transform.Find("heart3").GetComponent<Image>().sprite = deadHeart;
+                section.transform.Find("heart2").GetComponent<Image>().sprite = deadHeart;
+                section.transform.Find("heart1").GetComponent<Image>().sprite = deadHeart;
+                break;
+        }
     }
 }
